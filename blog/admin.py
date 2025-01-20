3a387a4a090544cedce16e49d4adba92c7d926bd
Erik Xalatyan <exalatyan123@gmail.com>
2025-01-21 01:35:59 +0400

    Admin setup for Blog

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
 # Register your models here.
+@admin.register(BlogCategory)
+class BlogCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'status', 'created_at', 'reading_time')
+    list_filter = ('status', 'category', 'created_at', 'author')
+    search_fields = ('title', 'content', 'excerpt')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'created_at'
+    raw_id_fields = ('author',)
+    inlines = [FAQInline]  # Add FAQInline here
+    fieldsets = (
+        ('Content', {
+            'fields': ('title', 'slug', 'excerpt', 'content', 'featured_image')
+        }),
+        ('Meta Data', {
+            'fields': ('meta_description', 'meta_keywords'),
+            'classes': ('collapse',)
+        }),
+        ('Categories', {  # Removed 'tags'
+            'fields': ('category',)
+        }),
+        ('Publishing', {
+            'fields': ('status', 'author', 'published_at')
+        })
+    )
+
